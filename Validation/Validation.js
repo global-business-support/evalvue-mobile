d516a189189d1f59f551251806ffbafcb076968d
Aman Chhalotre <152047708+Aman-Chhalotre@users.noreply.github.com>
2024-07-23 17:56:24 +0530

    user register validation updated
    user register validation updated

--- Validation/Validation.js
+++ Validation/Validation.js
@@ -1,46 +1,48 @@
-import regex from "../Pages/Regexconstant";
-export function ValidateEmail(email) {
-    var validateobj = {
-        isValid: true,
-        message: "hello",
-    };
-    if (!regex.Emailregex.test(email)) {
-        validateobj.isValid = false;
-        validateobj.message = "Invalid Email";
-    }
-    return validateobj;
-};
-export function ValidatePassword(password) {
-    var validateobj = {
-        isValid: true,
-        message: "hello",
-    };
+import regex from './Regex.js';
 
-    if (!regex.Passwordregex.test(password)) {
-        validateobj.isValid = false;
-        validateobj.message = "Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character.";
-    }
-    return validateobj;
-};
+export function ValidateEmail(email) {
+  var validateobj = {
+    isValid: true,
+    message: 'hello',
+  };
+  if (!regex.Emailregex.test(email)) {
+    validateobj.isValid = false;
+    validateobj.message = 'Invalid Email';
+  }
+  return validateobj;
+}
+export function ValidatePassword(password) {
+  var validateobj = {
+    isValid: true,
+    message: 'hello',
+  };
+
+  if (!regex.Passwordregex.test(password)) {
+    validateobj.isValid = false;
+    validateobj.message =
+      'Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character.';
+  }
+  return validateobj;
+}
 export function ValidateName(name) {
-    var validateobj = {
-        isValid: true,
-        message: "",
-    };
-    if (!regex.Nameregex.test(name)) {
-        validateobj.isValid = false;
-        validateobj.message = "Invalid Name";
-    }
-    return validateobj;
-};
+  var validateobj = {
+    isValid: true,
+    message: '',
+  };
+  if (!regex.Nameregex.test(name)) {
+    validateobj.isValid = false;
+    validateobj.message = 'Invalid Name';
+  }
+  return validateobj;
+}
 export function ValidateAddhar(addhar) {
-    var validateobj = {
-        isValid: true,
-        message: "",
-    };
-    if (!regex.Addharregex.test(addhar)) {
-        validateobj.isValid = false;
-        validateobj.message = "Invalid Aadhaar Number";
-    }
-    return validateobj;
-};
+  var validateobj = {
+    isValid: true,
+    message: '',
+  };
+  if (!regex.Addharregex.test(addhar)) {
+    validateobj.isValid = false;
+    validateobj.message = 'Invalid Aadhaar Number';
+  }
+  return validateobj;
+}
